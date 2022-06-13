f37d192b77201ab8db80dad0864489063e8dfb2c
Leonardo Abanto <leonardo_abanto@hotmail.com>
2022-06-12 21:56:29 -0500

    pasar html a ejs - login y registro

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-const express = require('express'); 
+const express = require('express');  //express
 const app = express();
-const path = require('path');             
-const port = 3000;
-const bodyParser = require('body-parser')                  
+const path = require('path');         
+const port = 3000; //puerto
+const bodyParser = require('body-parser')      //para formularios            
 
 app.use(express.static(path.join(__dirname, 'assets'))) // configurar archivos estaticos
 app.set('view engine', 'ejs') // configurar ejs template
-app.set('views', path.join(__dirname, '/views')) 
+app.set('views', path.join(__dirname, '/views'))  // configurar vistas
 app.use(bodyParser.json()) // para trabajar con formularios 
 app.use(bodyParser.urlencoded({
     extended: true
 })) // para trabajar con formularios
 
+//pagina principal
 app.get('/', (req, res) => {
     res.render('index', {
     })
 })
 
+//pagina login
+app.get('/login', (req, res) => {
+    res.render('login', {
+    })
+})
+
+//pagina registro
+app.get('/register', (req, res) => {
+    res.render('registro', {
+    })
+})
+
+//iniciar servidor
 app.listen(port, () => {            
     console.log(`Now listening on port ${port}`); 
 });
