ddd26d37525dd97a85a96060d5e601c30213d38a
Leonardo Abanto <leonardo_abanto@hotmail.com>
2022-06-14 16:41:21 -0500

    dao_ofertas

---
+++ models/dao_ofertas.js
@@ -0,0 +1,29 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database.sqlite');
+
+const {Oferta} = require("./Oferta.js")
+
+const queryOfertas = async () => {
+
+    ofertas = await new Promise(function(resolve,reject){
+        db.all('SELECT id, fecha, nombre_empresa, texto, puesto, id_usuario FROM Oferta', function(err, rows){
+        if (err){
+            reject (err)
+            return
+        }
+        resolve(rows)
+    })
+    })
+    return ofertas
+}
+
+async function getOfertas() {
+    publis = await queryOfertas()
+}
+
+const crearOferta = async(oferta) => {
+    var stmt = db.prepare('INSERT INTO Oferta (fecha, nombre_empresa, texto, puesto, id_usuario) VALUES (?,?,?,?,?)');
+    stmt.run([oferta.fecha,oferta.nombre_empresa,oferta.texto,oferta.puesto,oferta.id_usuario])
+}
+
+module.exports = {getOfertas, crearOferta} 
