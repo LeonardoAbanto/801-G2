c40b95f1b0da4a8bb694538707f99a371f49b55e
Leonardo Abanto <leonardo_abanto@hotmail.com>
2022-06-14 21:01:42 -0500

    boton publicar

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const path = require('path');
 const port = 3000; //puerto
 const bodyParser = require('body-parser')      //para formularios       
 
+const {Publicacion} = require("./models/Publicacion.js")
 const {Usuario} = require("./models/Usuario.js")
 const {crearUsuario, getUsuario, getUsuarios} = require("./models/dao_usuarios.js")
-const {getPublicaciones} = require("./models/dao_publicaciones.js")
+const {getPublicaciones, crearPublicacion} = require("./models/dao_publicaciones.js")
 
 app.use(express.static(path.join(__dirname, 'assets'))) // configurar archivos estaticos
 app.set('view engine', 'ejs') // configurar ejs template
@@ -26,18 +27,28 @@ app.use(session({
 
 //pagina principal
 app.get('/', async (req, res) => {
-    console.log(req.session.rol)
+    console.log(req.session.usuario)
     if(req.session.rol==null){
         res.redirect('/login')
     }else{
-        console.log(req.session.usuario)
         publicaciones = await getPublicaciones()
-        console.log(publicaciones)
         res.render('index', {
             rol: req.session.rol,
             usuario: req.session.usuario,
             publis: publicaciones
         })
+    }
+})
+
+//publicar
+app.post('/publicar', async (req, res) =>{
+    console.log(req.body.texto)
+    try{
+        let publicacion = new Publicacion(null,req.body.texto,1,req.session.usuario.id_usuario)
+        await crearPublicacion(publicacion)
+        res.redirect('/')
+    }catch (err){
+        console.log(err)
     }
 })
 
